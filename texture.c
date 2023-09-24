ff5f75c0396e8379b11bd492c102d6c396aef78f
mnilsson183 <morgan.nilsson7111@icloud.com>
2023-09-24 16:00:50 -0600

    added comments to raw mode code

--- texture.c
+++ texture.c
@@ -1,17 +1,23 @@
+
 #include <termios.h>
 #include <unistd.h>
 
 void enableRawMode(void)
 {
     // function to enter raw mode of the terminal
+
     struct termios raw;
 
+    // tcgetattr reads the terminal attributes
     tcgetattr(STDIN_FILENO, &raw);
 
-    // dont echo what the user types into the shell
+    // dont echo what the user types into the shell by modifying the flags
     raw.c_lflag &= ~(ECHO);
+
+    // now set the terminals attributes to reflect raw mode
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+
 int main(void)
 {
     enableRawMode();
