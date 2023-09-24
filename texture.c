e636bb14f3de6f5da228aafdb0a3e495bb8b0027
mnilsson183 <morgan.nilsson7111@icloud.com>
2023-09-24 16:10:39 -0600

    commit to exit raw mode on prgram exit

--- texture.c
+++ texture.c
@@ -1,20 +1,31 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+// global var tha is the default settings of our terminal
+struct termios orig_termios;
+
+void disableRawMode(void)
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 void enableRawMode(void)
 {
     // function to enter raw mode of the terminal
 
-    struct termios raw;
-
     // tcgetattr reads the terminal attributes
-    tcgetattr(STDIN_FILENO, &raw);
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    // atexit runs this code when the program exits
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
 
     // dont echo what the user types into the shell by modifying the flags
     raw.c_lflag &= ~(ECHO);
 
     // now set the terminals attributes to reflect raw mode
+    // tcsa flush waits for pending output to be printed to screen
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
