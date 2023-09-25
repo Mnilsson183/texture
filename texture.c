b2c61215ceaf4ef94045c8f52723b0e5b2735cc8
Morgan Nilsson <morgan.nilsson7111@icloud.com>
2023-09-25 13:33:35 -0600

    backstop windowsize

--- texture.c
+++ texture.c
@@ -70,20 +70,6 @@ void enableRawMode(void){
         terminate("tcsetattr");
     }
 }
-
-int getWindowSize(int* rows, int* columns){
-    struct winsize ws;
-
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
-        return -1;
-    } else{
-        *columns = ws.ws_col;
-        *rows = ws.ws_row;
-        return 0;
-    }
-}
-
-/** INPUT**/
 char editorReadKey() {
     int nread;
     char c;
@@ -96,6 +82,42 @@ char editorReadKey() {
     // if no issue return each key press
     return c;
 }
+
+int getCursorPosition(int* rows, int* columns){
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4){
+        return -1;
+    }
+    printf("/r/n");
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1){
+        if(iscntrl(c)){
+            printf("%d\r\n", c);
+        } else{
+            printf("%d ( %c )\r\n", c,c);
+        }
+    }
+    editorReadKey();
+    return -1;
+    
+}
+
+int getWindowSize(int* rows, int* columns){
+    struct winsize ws;
+
+    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12){
+            return getCursorPosition(rows, columns);
+        }
+        editorReadKey();
+        return -1;
+    } else{
+        *columns = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+/** INPUT**/
 
 void editorProcessKey(void){
     char c = editorReadKey();
