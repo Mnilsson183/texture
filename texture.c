fd43321a7f5c233b918f0015c601299cf4fc2a4d
Morgan Nilsson <morgan.nilsson7111@icloud.com>
2023-09-25 14:46:14 -0600

    abuf

--- texture.c
+++ texture.c
@@ -1,6 +1,7 @@
 // basic c text editor
 
 /** INCLUDES **/
+#include <string.h>
 #include <sys/ioctl.h>
 #include <stdbool.h>
 #include <errno.h>
@@ -124,6 +125,28 @@ int getWindowSize(int* rows, int* columns){
         *rows = ws.ws_row;
         return 0;
     }
+}
+/* APPEND BUFFER */
+struct abuf{
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT{NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len){
+    char* new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL){
+        return;
+    }
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab){
+    free(ab->b);
 }
 
 /** INPUT**/
