538f3224bf146b3ba4944c1e3ca396efb873b03e
Morgan Nilsson <morgan.nilsson7111@icloud.com>
2023-09-25 10:15:50 -0600

    Added misc comments and formatting

--- texture.c
+++ texture.c
@@ -1,3 +1,4 @@
+// INCLUDES
 #include <stdbool.h>
 #include <errno.h>
 #include <ctype.h>
@@ -5,11 +6,16 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+// INCLUDES
 
-// global var tha is the default settings of our terminal
+
+// TERMINAL CONTROLS
+
+// global var tha is the default settings of terminal
 struct termios orig_termios;
 
 void terminate(const char *s){
+    // function to deal with the error outputs
     perror(s);
     exit(1);
 }
@@ -29,24 +35,29 @@ void enableRawMode(void)
     if(tcgetattr(STDIN_FILENO, &orig_termios) == -1){
         terminate("tcgetattr");
     }
-    // atexit runs this code when the program exits
+    // atexit disable the raw mode once the program finishes running
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
 
-    // dont echo what the user types into the shell by modifying the flags
+    // terminal flags and other specifiers that allow out program to output
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    
+    // set the parameters of the read function
+    // VMIN is the number of characters read before stopping reading
     raw.c_cc[VMIN] = 0;
+    // VTIME sets the timeout of the terminal read function
     raw.c_cc[VTIME] = 1;
 
-    // now set the terminals attributes to reflect raw mode
+    // Set the terminals attributes to reflect raw mode
     // TCSAFLUSH waits for pending output to be printed to screen
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1){
         terminate("tcsetattr");
     }
 }
+// TERMINAL CONTROLS
 
 int main(void)
 {
@@ -58,11 +69,16 @@ int main(void)
         if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN){
             terminate("read");
         }
-            if(iscntrl(c)){
-                printf("%d\r\n", c);
-            } else{
-                printf("%d( %c )\r\n", c, c);
-            }
+
+        if(iscntrl(c)){
+            // print the ascii of any control characters followed by a carrige return and a new line characters
+            printf("%d\r\n", c);
+        } else{
+            // print both the ascii and the value followed by a carrige return and a new line characters
+            printf("%d( %c )\r\n", c, c);
+        }
+
+        // q is the exit condition of the program
         if (c == 'q'){
             break;
         }  
