011af01bde375e99100dfaa5655178eb392e52a3
Morgan Nilsson <morgan.nilsson7111@icloud.com>
2023-09-25 11:38:42 -0600

    get terminal win size

--- texture.c
+++ texture.c
@@ -1,6 +1,7 @@
 // basic c text editor
 
 /** INCLUDES **/
+#include <sys/ioctl.h>
 #include <stdbool.h>
 #include <errno.h>
 #include <ctype.h>
@@ -69,7 +70,18 @@ void enableRawMode(void)
         terminate("tcsetattr");
     }
 }
-// TERMINAL CONTROLS
+
+int getWindowSize(int* rows, int* columns){
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
+        return -1;
+    } else{
+        *columns = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
 
 /** INPUT**/
 char editorReadKey() {
