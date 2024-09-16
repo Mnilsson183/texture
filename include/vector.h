04a291c31b2db15433f85f098656a0ff99b57297
mnilsson183 <morgan.nilsson7111@icloud.com>
2024-09-16 13:42:10 -0600

    error checking value on vector

--- include/vector.h
+++ include/vector.h
@@ -1,14 +1,22 @@
 #include <stdlib.h>
 
+typedef enum {
+    OK,
+    UNKNOWN,
+    FAILED,
+} VectorEnum;
+
 struct vector {
     int element_size;
     int num_elements;
     int max_elements;
+    VectorEnum status;
     void** data;
 };
 
 struct vector* vector_init_size(struct vector* self, int element_size, int initSize);
 struct vector* vector_init(struct vector* self, int element_size);
+int vector_size(struct vector* self);
 void vector_term(struct vector* self);
 void vector_term_ptrs(struct vector* self);
 void vector_add(struct vector* self, void* data);
--- src/vector.c
+++ src/vector.c
@@ -3,6 +3,7 @@
 #include <strings.h>
 
 
+
 struct vector* vector_init_size(struct vector* self, int element_size, int initSize) {
     if (initSize <= 0) return NULL;
     if (element_size <= 0) return  NULL;
@@ -14,6 +15,7 @@ struct vector* vector_init_size(struct vector* self, int element_size, int initS
         return NULL;
     }
     self->num_elements = 0;
+    self->status = OK;
     return self;
 }
 
@@ -23,12 +25,19 @@ struct vector* vector_init(struct vector* self, int element_size) {
 
 // will return NULL for bad resize values
 void vector_resize(struct vector* self, int newSize) {
-    if (newSize <= 0 || newSize < self->num_elements) return;
+    self->status = UNKNOWN;
+    if (newSize <= 0 || newSize < self->num_elements) {
+        self->status = FAILED;
+    }
     self->max_elements = newSize;
     void* newData = malloc(self->element_size * newSize);
+    if (newData == NULL) {
+        self->status = FAILED;
+    }
     memcpy(newData, self->data, self->element_size * self->num_elements);
     free(self->data);
     self->data = newData;
+    self->status = OK;
 }
 
 void vector_term(struct vector* self) {
@@ -48,12 +57,15 @@ void vector_add(struct vector* self, void* data) {
     if (self->num_elements + 1 == self->max_elements) {
         vector_resize(self, self->max_elements*2);
     }
+    if (self->status != OK) {
+        return;
+    }
     self->data[self->num_elements] = data;
     self->num_elements++;
 }
 
 void* vector_get(struct vector* self, int index) {
-    if (index > self->num_elements) {
+    if (index > self->num_elements || index < 0) {
         return NULL;
     }
     return self->data[index];
